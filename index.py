b2bf62e6c6868a026ae865da8bf1a956294c4173
Roeland Kuipers <roeland@boul.nl>
2020-01-05 11:16:19 +0100

    test

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World',
+        'output': 'Hello Worlds',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
